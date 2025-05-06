aaa6007f227b9cf22788cd2c0c16bed8404759d5
jolainejones27 <jolainejones1227@gmail.com>
2025-05-06 14:07:46 -0400

    added notes

--- version-0/src/components/CountryCard.jsx
+++ version-0/src/components/CountryCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../App.css';
 
+// Defines a component that accepts a country object as a prop
 function CountryCard({ country }) {
+  // optional chaining (accesses values witout causing errors if its not there and the OR (||) operator tells it to use the right side as a default
   const name = country.name?.common || "No name";
   const capital = country.capital?.[0] || "No capital";
   const region = country.region || "No region";
   const population = country.population?.toLocaleString() || "No data";
   const flagPng = country.flags?.png || "";
 
+  // shows how the UI should look
   return (
     <div className="country-card">
       <img src={flagPng} alt={`Flag of ${name}`} />
@@ -21,3 +24,5 @@ function CountryCard({ country }) {
 
 export default CountryCard;
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
--- version-0/src/pages/Home.jsx
+++ version-0/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CountryCard from '../components/CountryCard';
-import localData from '../../localData';
+import localData from '../../localData'; // static (easy to access) country data
 import '../App.css';
 
 function Home() {
@@ -8,6 +8,7 @@ function Home() {
     <div>
       <h1></h1>
       <div className="countries-list">
+        {/* // This map method loops through the items in localData and renders a card for each country Index is used as a unique key for each card. */}
         {localData.map((country, index) => (
           <CountryCard key={index} country={country} />
         ))}
